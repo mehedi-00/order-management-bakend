f73a7071484c0491424412c94ec6d9e099c5ceb0
mehedidev <mdmehedi23234@gmail.com>
2023-11-25 15:16:32 +0600

    some modify

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import { userRouter } from './app/modules/user/user.route'
 const app: Application = express()
@@ -12,6 +12,13 @@ app.use('/api/users', userRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.use('*', (req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: 'router is not available',
+  })
 })
 
 export default app
